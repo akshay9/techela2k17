90ce51a65ecd325214023ce6703e857c2546dc57
Akshay Naik <akshay9_5@yahoo.co.in>
2017-09-03 16:51:49 +0530

    Added Basic Homepage.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,16 @@
 
 $(document).ready(function(){
 
+    $('#home').find('canvas').constellation({
+        star: {
+            width: 3
+        },
+        line: {
+            color: 'rgba(255, 0, 0, .5)'
+        },
+        radius: 250
+    });
+
     // init controller
     var controller = new ScrollMagic.Controller();
 
@@ -11,6 +21,7 @@ $(document).ready(function(){
     var logoTimeline = new TimelineMax()
         .to("#home .logo", 0.5,  {left: "5", position: "fixed", ease: Power3.easeOut }, 0)
         .to("#home .logo", 0.5,  {top: "5" }, 0)
+        .to("#home .description", 0.5,  {width: "0", opacity:0 }, 0)
         .to(window, 0.5,  {scrollTo: "#events"}, 0);
 
     var scene = new ScrollMagic.Scene({
@@ -18,7 +29,7 @@ $(document).ready(function(){
         duration: 0
     })
         .setTween(logoTimeline) // trigger a TweenMax.to tween
-        .addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
+        //.addIndicators({name: "1 (duration: 0)"}) // add indicators (requires plugin)
         .addTo(controller);
 
     var eventsTimeline = new TimelineMax()
@@ -35,14 +46,14 @@ $(document).ready(function(){
         .setPin("#events")
         .setClassToggle("#eventLink", "active")
         .setTween(eventsTimeline) // trigger a TweenMax.to tween
-        .addIndicators({name: "2 (duration: 300%)"}) // add indicators (requires plugin)
+        //.addIndicators({name: "2 (duration: 300%)"}) // add indicators (requires plugin)
         .addTo(controller);
     var eventsScene2 = new ScrollMagic.Scene({
         triggerElement: "#events",
         triggerHook: "onLeave",
         duration: "400%"
     })
-        .setClassToggle("nav.navbar", "navbar-dark")
+        //.setClassToggle("nav.navbar", "navbar-dark")
         .addTo(controller);
 
 

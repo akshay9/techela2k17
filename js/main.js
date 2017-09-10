25abf1c89869e8ffa00b7fd09e6720c9d00731d4
Akshay Naik <akshay9_5@yahoo.co.in>
2017-09-10 18:30:58 +0530

    -Added new Techela Logo & Font -Basic Mobile support added -Social Links updated -Events img updates -Minor theme updates

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ $(document).ready(function(){
             width: 3
         },
         line: {
-            color: 'rgba(255, 0, 0, .6)'
+            color1: 'rgba(255, 0, 0, .6)',
+            color2: 'rgba(7, 128, 196, .6)',
+            color: 'rgba(8, 116, 168, .5)'
         },
         radius: 250
     });
@@ -17,9 +19,9 @@ $(document).ready(function(){
 
 
     var logoTimeline = new TimelineMax()
-        .to("#home .logo", 0.5,  {left: "10", position: "fixed", ease: Power3.easeOut }, 0)
-        .to("#home .logo", 0.5,  {top: "10" }, 0)
-        .to("#home .logo h1", 0.5,  {'font-size': "2.5em" }, 0)
+        .to("#home .logo", 0.5,  {left: "5", position: "fixed", ease: Power3.easeOut }, 0)
+        .to("#home .logo", 0.5,  {top: "5" }, 0)
+        .to("#home .logo img", 0.5,  {'max-height': "85" }, 0)
         .to("#home .description", 0.5,  {width: "0", opacity:0 }, 0)
         .to("#register-btn", 0.5,  {opacity:0 }, 0)
         .to(window, 0.5,  {scrollTo: "#events"}, 0);
@@ -43,24 +45,29 @@ $(document).ready(function(){
         .fromTo(".layer0.page", 4, {x: "0%"}, {x: "-" + (pages * 100) +"%", ease: Linear.easeNone}, 0);
         // .fromTo(".layer0.page", 4, {x: "0%"}, {x: (-16670+window.innerWidth) + "", ease: Linear.easeNone}, 0);
 
-    var eventsScene = new ScrollMagic.Scene({
-        triggerElement: "#events",
-        triggerHook: "onLeave",
-        duration: "400%"
-    })
-        .setPin("#events")
-        // .setClassToggle("#eventLink", "active")
-        .setTween(eventsTimeline) // trigger a TweenMax.to tween
-        //.addIndicators({name: "2 (duration: 300%)"}) // add indicators (requires plugin)
-        .addTo(controller);
-    // var eventsScene2 = new ScrollMagic.Scene({
-    //     triggerElement: "#events",
-    //     triggerHook: "onLeave",
-    //     duration: "400%"
-    // })
-    //     //.setClassToggle("nav.navbar", "navbar-dark")
-    //     .addTo(controller);
+    if(window.innerWidth > 1200) {
 
+        var eventsScene = new ScrollMagic.Scene({
+            triggerElement: "#events",
+            triggerHook: "onLeave",
+            duration: "400%"
+        })
+            .setPin("#events")
+            // .setClassToggle("#eventLink", "active")
+            .setTween(eventsTimeline) // trigger a TweenMax.to tween
+            //.addIndicators({name: "2 (duration: 300%)"}) // add indicators (requires plugin)
+            .addTo(controller);
+        // var eventsScene2 = new ScrollMagic.Scene({
+        //     triggerElement: "#events",
+        //     triggerHook: "onLeave",
+        //     duration: "400%"
+        // })
+        //     //.setClassToggle("nav.navbar", "navbar-dark")
+        //     .addTo(controller);
+    } else {
+        $("#events").find(".page").removeClass("page");
+        $("#events").css("min-height", "100%!important")
+    }
 
 
     // Menu Handler
